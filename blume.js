d25c7dfb1c3caaca06607932ddd426e0ccdd0a7e
infinte15 <finn.deuschle1@gmail.com>
2025-02-14 21:18:07 +0100

    Startseite/Blume

---
+++ blume.js
@@ -0,0 +1,12 @@
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "Space") {
+        window.location.href = "klasse.html"; // Change to your next page
+    }
+});
