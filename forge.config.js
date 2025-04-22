573f2bb854549592328e3c9e0d96aa1bb2bf10d8
infinte15 <finn.deuschle1@gmail.com>
2025-04-22 18:41:18 +0200

    1

--- forge.config.js
+++ forge.config.js
@@ -2,17 +2,31 @@ const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
       config: {
         certificateFile: './cert.pfx',
         certificatePassword: process.env.CERTIFICATE_PASSWORD
-      },
+      }
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
     },
   ],
-
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
