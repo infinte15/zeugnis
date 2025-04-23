f8eebda4b5868d3e50910ccad9406514a5159ed3
infinte15 <finn.deuschle1@gmail.com>
2025-04-23 16:06:04 +0200

    5

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const { app, BrowserWindow } = require('electron')
-require('update-electron-app')()
+const { app, BrowserWindow } = require('electron');
+const updateElectronApp = require('update-electron-app')();
+
+updateElectronApp();
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1500,
